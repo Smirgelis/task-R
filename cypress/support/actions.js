64148075db2586c5eeaf3f4075fbb1915df38066
Laurynas Runkauskas <laurynas@ancient.gg>
2025-03-13 20:18:58 +0200

    task done

--- cypress/support/actions.js
+++ cypress/support/actions.js
@@ -2,5 +2,5 @@ import testObjectIDs from "../support/objectIDs";
 export async function useSearch(word) {
     cy.get(testObjectIDs.SEARCH_FIELD).eq(0)
         .type(`${word}{enter}`);
-    cy.url().should('include', '/find/flowers');
+    cy.url().should('include', `/find/${word}`);
 }
